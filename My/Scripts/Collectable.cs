49570faf15afa0dbc3f5a93f7e5fce2cce83c37a
Noeyuyg <dnlrbdus31@gmail.com>
2024-02-17 20:00:38 +0900

    퍼즐 카테고리 분류

--- My/Scripts/Collectable.cs
+++ My/Scripts/Collectable.cs
@@ -8,13 +8,23 @@ public class Collectable : MonoBehaviour
     public Vector3 posToGo;
     public PuzzleItemData item;
 
+    private Button myBtn;
+    private Image myimage;
+
+    private void Start()
+    {
+        myBtn = GetComponent<Button>();
+        myimage = GetComponent<Image>();
+        myimage.sprite = item.puzzleIcon;
+    }
+
     public void ClickBtn()
     {
         bool wasPickUpTetris = false;
         wasPickUpTetris = TetrisSlot.instanceSlot.addInFirstSpace(item); //add to the bag matrix.
         if (wasPickUpTetris) // took
         {
-            gameObject.GetComponent<Button>().interactable = !wasPickUpTetris;
+            myBtn.interactable = !wasPickUpTetris;
         }
     }
 }
---
+++ My/Scripts/PuzzleView.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleView : MonoBehaviour
+{
+    public GameObject[] puzzleBtn;
+
+    int x = 1;
+    int y = 1;
+    private void Start()
+    {
+        PuzzleViewClick();
+    }
+
+    public void OnClickX(int x)
+    {
+        this.x = x;
+    }
+
+    public void OnClickY(int y)
+    {
+        this .y = y;
+    }
+
+    public void PuzzleViewClick()
+    {
+        for (int i = 0; i < puzzleBtn.Length; i++)
+        {
+            int contX = (int)puzzleBtn[i].GetComponent<Collectable>().item.puzzleSize.x;
+            int contY = (int)puzzleBtn[i].GetComponent<Collectable>().item.puzzleSize.y;
+
+            if (contX == x && contY == y)
+            {
+                puzzleBtn[i].SetActive(true);
+            }
+            else
+            {
+                puzzleBtn[i].SetActive(false);
+            }
+        }
+    }
+}
+
